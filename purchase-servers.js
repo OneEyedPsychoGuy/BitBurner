2819d178eb6d77571ba149fb41fe72bde5f1bbe8
Jordan Meredith <53634067+OneEyedPsychoGuy@users.noreply.github.com>
2023-04-15 21:49:29 -0700

    Create purchase-servers.js
    Buys additional in-game servers with max ram capacity until maximum number of servers can be bought.

---
+++ purchase-servers.js
@@ -0,0 +1,20 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    
+    //Set RAM here
+    const ram = ns.getPurchasedServerMaxRam();
+    let i = ns.getPurchasedServers().length;
+
+    while (i < ns.getPurchasedServerLimit()) 
+    {
+        if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) 
+		{
+	        ns.purchaseServer("server-node-" + i, ram)
+            ++i;
+        }
+        //Prevents Bitburner from freezing
+        await ns.sleep(1000);
+    }
+
+    ns.tprint("Maximum Servers Purchased!");
+}
