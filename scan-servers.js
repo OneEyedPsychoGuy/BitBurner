2ca08971d50cfb16326602e3163df73323534bb3
Jordan Meredith <53634067+OneEyedPsychoGuy@users.noreply.github.com>
2023-04-15 21:53:19 -0700

    Create scan-servers.js
    Scans all servers in-game, including ones you own (bought servers and "home" server). Comprises a list in the form of an array and sends that to write-server-info.js. Needs to be isolated later on.

---
+++ scan-servers.js
@@ -0,0 +1,17 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	let servers = ["home"];
+
+	for (let server of servers) {
+		let scans = ns.scan(server);
+
+		for (let scan of scans) {
+			if (!servers.includes(scan)) {
+				servers.push(scan);
+			}
+		}
+	}
+
+	const scriptName = "write-server-info.js"
+	ns.exec(scriptName, "home", 1, ...servers);
+}
