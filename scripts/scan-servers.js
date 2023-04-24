5058fcd4dee9d564fa66745f00a4c12825fbee0b
Jordan Meredith <53634067+OneEyedPsychoGuy@users.noreply.github.com>
2023-04-24 08:43:37 -0700

    Created Functions for Fetching Server Hostnames

--- scripts/scan-servers.js
+++ scripts/scan-servers.js
@@ -1,5 +1,11 @@
 /** @param {NS} ns */
-export async function main(ns) {
+export function main(ns) {
+	ns.tprint("All: " + fetchAllServers(ns));
+	ns.tprint("Targets: " + fetchTargetServers(ns));
+	ns.tprint("Owned: " + fetchOwnedServers(ns));
+}
+
+export function fetchAllServers(ns) {
 	let servers = ["home"];
 
 	for (let server of servers) {
@@ -12,8 +18,13 @@ export async function main(ns) {
 		}
 	}
 
-	/*
-	const scriptName = "write-server-info.js"
-	ns.exec(scriptName, "home", 1, ...servers);
-	*/
+	return servers;
+}
+
+export function fetchTargetServers(ns) {
+	return fetchAllServers(ns).filter(server => !(server.startsWith("server") || server === "home"));
+}
+
+export function fetchOwnedServers(ns) {
+	return fetchAllServers(ns).filter(server => server.startsWith("server") || server === "home");
 }
